5295f104235f7c2ec3e5d2d5a2111c280a3bfce3
Joseph Omotade <josephomotade0@gmail.com>
2023-06-24 11:26:05 +0100

    commit

--- pages/meet/[id].tsx
+++ pages/meet/[id].tsx
@@ -51,10 +51,12 @@ const VideoCall = () => {
 
   useEventListner("lobby:initialized", () => {
     iframeApi.initialize({
-      redirectUrlOnLeave: "https://localhost:3000/dashboard/feed",
+      redirectUrlOnLeave:
+        "https://tradeverse-particle.vercel.app/dashboard/feed",
       wallets: ["metamask"],
-      background: "https://gateway.pinata.cloud/ipfs/QmWCec9nWPf7KnaBGeaWtVoPT8dBEXSWXjLhhb47RTfPUD?_gl=1*1rqjpoc*rs_ga*NzczNDkyOTU3LjE2ODc0NzY0MjE.*rs_ga_5RMPXG14TE*MTY4NzQ3NjQyMC4xLjEuMTY4NzQ3NjcyOC43LjAuMA..",
-      gradientAndMesh: false
+      background:
+        "https://gateway.pinata.cloud/ipfs/QmWCec9nWPf7KnaBGeaWtVoPT8dBEXSWXjLhhb47RTfPUD?_gl=1*1rqjpoc*rs_ga*NzczNDkyOTU3LjE2ODc0NzY0MjE.*rs_ga_5RMPXG14TE*MTY4NzQ3NjQyMC4xLjEuMTY4NzQ3NjcyOC43LjAuMA..",
+      gradientAndMesh: false,
     });
   });
 
@@ -74,7 +76,6 @@ const VideoCall = () => {
       <HuddleIframe
         roomUrl={`https://iframe.huddle01.com/${id}`}
         className="w-full aspect-video h-[960px] mt-[12px]"
-
       />
       <div className="fixed top-0 w-full">
         <Navbar />
